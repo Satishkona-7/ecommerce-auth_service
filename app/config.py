11fc6c5792682fce9bc440d326e22240ffa2b8eb
Satishkona-7 <konasatish4444@gmail.com>
2026-06-05 16:49:12 +0530

    cheanged env format

--- app/config.py
+++ app/config.py
@@ -3,14 +3,14 @@ import os
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI")
+MONGO_URI = os.getenv("MONGO-URI")
 
-DATABASE_NAME = os.getenv("DATABASE_NAME")
+DATABASE_NAME = os.getenv("DATABASE-NAME")
 
-JWT_SECRET = os.getenv("JWT_SECRET")
+JWT_SECRET = os.getenv("JWT-SECRET")
 
-JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
+JWT_ALGORITHM = os.getenv("JWT-ALGORITHM")
 
 TOKEN_EXPIRE_MINUTES = int(
-    os.getenv("TOKEN_EXPIRE_MINUTES", 60)
+    os.getenv("TOKEN-EXPIRE-MINUTES", 60)
 )
